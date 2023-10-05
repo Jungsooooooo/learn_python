9ca50708b783af1e74b515a2d960de2a4139f5f7
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2023-10-05 17:56:30 +0900

    주석, 입출력 및 변수 사용 기초 문법

--- learn_python.py
+++ learn_python.py
@@ -1,1 +1,33 @@
 print("first commit")
+
+#주석 다는 방법 (한줄)
+"""
+여러줄 주석
+"""
+
+print("안녕하세요")
+
+# 들여쓰기가 아주 중요하다
+n=10
+if n%2 ==0:
+    print('even')
+    print('짝수')
+else :
+    print('odd')
+    print('홀수')
+
+
+#변수 사용
+#영어와 _가 첫번째로 쓰임.
+#대소문자로 구문함.
+a=10
+b='나는b'
+c=5
+print(a)
+print(b)
+print(a+c)
+
+#표준 입출력
+print()
+name =input('이름을 입력해주세요') #입력
+print(name)
