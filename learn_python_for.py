5081bc89ccc2be231f5fa810c5ff000f157d0505
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2023-10-11 16:53:13 +0900

    for 와 range

---
+++ learn_python_for.py
@@ -0,0 +1,29 @@
+# for
+for i in range(10):
+    print(i)
+
+for i in range(1,11):
+    print(i)
+
+for i in range(3,10,3):
+    print(i)
+# range
+# range(11) -> 0부터 10까지 1씩 증가
+# range(1,11) => 1부터 10까지 1씩 증가
+# range(1,11,2) => 1부터 10까지 2씩 증가
+
+#0,1,2,3,4 뽑기
+print(list(range(5)))
+
+#1~10 값
+print(list(range(1,11)))
+
+#3,6,9
+print(list(range(3,10,3)))
+
+#5 4 3 2 1
+print(list(range(5,0,-1)))
+
+#10,5,0,-5,-10
+print(list(range(10,-11,-5)))
+
