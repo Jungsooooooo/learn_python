8c262c60059890534571879965c9b801919ecea1
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2023-10-16 15:51:45 +0900

    python 숫자 문자 체크 하기

---
+++ learn_pythoon_check.py
@@ -0,0 +1,27 @@
+# 문자 체크
+# 긴 문장과 한문자를 이별갑ㄷ아
+# t가 text안에 포함되어 있는지 확인
+
+# text= input('text:')
+
+# check = False
+
+# for i in text:
+#     if i == 't':
+#         check= True
+
+# print(check)            
+
+# 숫자 체크
+# 5개의 숫자를 입력받아 리스트를 만들고
+# n을 입력받아 리스트 값에 n이 있는지 확인
+
+li = list(map(int,input('li:').split()))
+n = int(input('n:'))
+
+check1 = False
+
+for i in li:
+    if i == n:
+        check1= True
+print(check1)        
